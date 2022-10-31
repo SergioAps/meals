16077ad40b784a3c060a38cd7d3171e23cb6a5f7
Eduardo Aguilar <laloaps@gmail.com>
2022-10-31 14:54:27 -0400

    Validate food name is present

--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class FoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name is not blank" do
+    food = Food.create
+
+    assert food.invalid?
+    assert_equal ["can't be blank"], food.errors[:name]
+  end
 end
