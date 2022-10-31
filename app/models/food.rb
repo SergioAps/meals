e29720382d34cba979732696128a4f94492985f1
Eduardo Aguilar <laloaps@gmail.com>
2022-10-31 15:34:18 -0400

    Food grams is an integer

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name, case_sensitive: false
+  validates_numericality_of :grams, only_integer: true, allow_nil: true
 end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -21,4 +21,21 @@ class FoodTest < ActiveSupport::TestCase
     assert food.invalid?
     assert_equal ["has already been taken"], food.errors[:name]
   end
+
+  test "grams can be blank" do
+    food = Food.new(name: "bistec de res")
+
+    assert food.valid?
+  end
+
+  test "grams has to be an integer" do
+    food = Food.new(name: "bistec de res", grams: "1 plate")
+
+    assert food.invalid?
+    assert_equal ["is not a number"], food.errors[:grams]
+
+    food = Food.new(name: "bistec de res", grams: "1.0")
+    assert food.invalid?
+    assert_equal ["must be an integer"], food.errors[:grams]
+  end
 end
